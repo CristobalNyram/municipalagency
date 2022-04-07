9afa9ab8cf4eafbea53b28d11c8c9371c05b9e0b
cristobal nyram <crisco.nyram@gmail.com>
2022-04-07 08:13:07 -0500

    added footer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,4 +12,21 @@
             </div>
         </div>
     </div>
+    
+
+<footer class="text-center text-white" style="background-color: #0a4275;">
+  <div class="container p-6">
+    <div class="">
+      <p class="flex justify-center items-center">
+        <span class="mr-4">Sistema de gestión de para San Sebastian De La Frontera.</span>
+       
+      </p>
+    </div>
+  </div>
+
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="https://dvtecam.com/"> <strong>DvTecam</strong> </a>
+  </div>
+</footer>
 </x-app-layout>
