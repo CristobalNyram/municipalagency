26350b7271e9a62515ecbc5a370acfde7354dc42
cristobal nyram <crisco.nyram@gmail.com>
2022-04-16 19:51:42 -0500

    mejorarmos el modal

--- resources/views/crud/moneybox/index.blade.php
+++ resources/views/crud/moneybox/index.blade.php
@@ -93,25 +93,39 @@
 
 </section>
 
-<div class="modal fade" id="moneybox_add-modal" role="dialog">
+<!-- START MODALS-------------------------------------------------------------------------------------------------------START MODALS -->
+
+
+<div class="modal fade" id="moneybox_add-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Regitrar caja: </h5>
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Regitrar caja</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <p> folio : #</p>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <input type="text" id="form3" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="form3">Nombre</label>
+        </div>
+
+        <div class="md-form mb-4">
+          <input type="email" id="form2" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="form2">Descripción</label>
+        </div>
+
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Guardar registro</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      <div class="modal-footer d-flex justify-content-center">
+        <button class="btn btn-indigo bg-success">Registrar <i class="fas fa-paper-plane-o ml-1"></i></button>
       </div>
     </div>
   </div>
 </div>
+
+<!-- END MODALS-------------------------------------------------------------------------------------------------------END MODALS MODALS -->
 @stop
 
 @section('css')
